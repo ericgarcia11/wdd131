973356f35a65337680718b8ce010b851a8e13270
Eric Garcia <133874193+ericgarcia11@users.noreply.github.com>
2025-05-14 01:46:43 -0300

    done

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,17 +5,13 @@ yearSpan.textContent = currentYear;
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last modified: ${lastModified}`;
 
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
-const navLinks = document.querySelectorAll('.navigation a');
+// const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("#navigation");
+const navLinks = document.querySelectorAll('#navigation a');
 const imgBlurs = document.querySelectorAll('.img-blur');
 const page_title = document.getElementById('page_title');
 const headerTitle = document.querySelector('h1');
-
-// hamButton.addEventListener("_______", () => {
-// 	navigation.classList.toggle("open");
-// 	hamButton.classList.toggle("open");
-// });
+const hamburgerButton = document.getElementById('hamburgerButton');
 
 navLinks.forEach(link =>{
 	link.addEventListener('click', function(){
@@ -35,9 +31,7 @@ navLinks.forEach(link =>{
         }
 
         imgBlurs.forEach(img => img.setAttribute('src', newImage));
-
 		this.classList.add('active');
-
         page_title.innerHTML = link.textContent;
 	});
 });
@@ -50,3 +44,14 @@ headerTitle.addEventListener('click', function(){
     navLinks[0].setAttribute('class','active');
     page_title.innerHTML = 'Home';
 })
+
+hamburgerButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamburgerButton.textContent = navigation.classList.contains("open") ? "✖" : "☰";
+    if (navigation.classList.contains("open")){
+        document.querySelector('header').removeChild(hamburgerButton);
+        navigation.insertBefore(hamburgerButton, navigation.firstChild);
+    } else {
+        document.querySelector('header').appendChild(hamburgerButton);
+    }
+});
