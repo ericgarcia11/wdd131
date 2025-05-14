49131c3a37217d41e2a1b28c4b951e2619273fd2
Eric Garcia <133874193+ericgarcia11@users.noreply.github.com>
2025-05-13 22:34:20 -0300

    a

---
+++ scripts/temples.js
@@ -0,0 +1,6 @@
+const yearSpan = document.querySelector("footer span");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modified: ${lastModified}`;
