2fd20239c4192fb766c394a89fabdce681c28a37
Eric Garcia <133874193+ericgarcia11@users.noreply.github.com>
2025-06-02 23:02:25 -0300

    a

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,16 @@
+const wireframe_title = document.getElementById("wireframe_title");
+const wireframe_img = document.getElementById("wireframe_img");
+
+function updateWireframe(){
+    if (window.innerWidth > 740) {
+        wireframe_title.textContent = "Desktop View:";
+        wireframe_img.setAttribute("src", "../../images/desktop.png");
+    } else {
+        wireframe_title.textContent = "Mobile View:";
+        wireframe_img.setAttribute("src", "../../images/mobile.png");
+    }
+}
+
+updateWireframe();
+
+window.addEventListener("resize", updateWireframe);
